0cc2931fe23a11f346f6e74e253bf1edbcccb022
Pavlo <135157804+Pahanexe@users.noreply.github.com>
2025-06-04 17:53:45 +0300

    Update balancer_test.go

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -13,7 +13,7 @@ const (
 	requestsCount  = 15
 	timeout        = 5 * time.Second
 	retryInterval  = 20 * time.Second
-	maxRetries     = 30
+	maxRetries     = 5
 )
 
 func TestLeastConnectionsDistribution(t *testing.T) {
